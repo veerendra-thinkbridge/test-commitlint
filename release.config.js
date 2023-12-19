f7db8eb54d165a4894f8390bedb851b7bab621e0
veerendra-thinkbridge <veerendra.d@thinkbridge.in>
2023-12-19 10:49:10 +0530

    ci: prod config

--- release.config.js
+++ release.config.js
@@ -1,15 +1,23 @@
-module.exports = {
-    branches: ["main"],
-    plugins: [
+const config = {
+  branches: [
+    "main", 
+   { name: 'prod', prerelease: 'prod' },
+   { name: 'qa', prerelease: 'qa' },
+   { name: 'dev', prerelease: 'dev' }
+  ],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
     ["@semantic-release/git", {
-            "assets": ["CHANGELOG.md", "package.json"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-          }]
-    ],
+      "assets": ["CHANGELOG.md", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
+  ],
 };
+
+module.exports = config;
+
     
